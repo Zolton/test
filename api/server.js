108633c055b60eafdf1c5125ab9d028d8280d022
Zolton <gmzesq@gmail.com>
2019-08-08 13:26:52 -0400

    Typo correction

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const server = express()
 
-server.get("/", (req, res)=>{res.status(200).json({jargon: provess.env.JOTD})})
+server.get("/", (req, res)=>{res.status(200).json({jargon: process.env.JOTD})})
 
 module.exports = server;
